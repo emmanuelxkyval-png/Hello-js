06c26a3b7028dc2838a8c12196886a52adb7c3c0
Ekwem Chidumaga Emmanuel <emmanuelxkyval@gmail.com>
2025-10-10 16:16:31 +0100

    Initial commit: My assignment

---
+++ intro.js
@@ -0,0 +1,5 @@
+const name = 'Ekwem Chidumaga Emmanuel';
+const age = 18;
+const favoriteProgrammingLanguage = 'JavaScript';
+
+console.log(`Hello, my name is ${name}. I am ${age} years old and my favorite programming language is ${favoriteProgrammingLanguage}.`);
