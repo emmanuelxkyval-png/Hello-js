af7b642d0ecada28e41256d8cba742c7baf8720c
Ekwem Chidumaga Emmanuel <emmanuelxkyval@gmail.com>
2025-10-19 01:18:24 +0100

    week-2-node-express

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.get('/user/:id', (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
